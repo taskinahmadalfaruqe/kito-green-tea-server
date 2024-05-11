ab542145fb0c16950dac6158ca0bfab9024e7678
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-05-11 16:47:42 +0600

    [Backend Intrigation]: Add Backend setup and data intrigation

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// // user = e-shop-bd
+// // pass= UBShkNROi2oq4o2p
+
+// // SbqShhMkstQwducO
+// // taskinahmadalfaruqe
+
+
+
 
 const express = require('express');
 const cors = require('cors');
@@ -6,18 +14,25 @@ const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
+//CORS CONFIG FILE
+const corsConfig = {
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://e-shopbd-server.vercel.app/',
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
+}
 
 // middleware 
 app.use(cors())
 app.use(express.json());
 
-// user = e-shop-bd
-// pass= UBShkNROi2oq4o2p
-
+// const uri = "mongodb+srv://taskinahmadalfaruqe:SbqShhMkstQwducO@cluster0.5tob3mx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const uri = "mongodb+srv://e-shop-bd:UBShkNROi2oq4o2p@cluster0.1gxcng8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,22 +41,47 @@ const client = new MongoClient(uri, {
   }
 });
 
+let trackOrder = 1000;
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+    const productsCollection = client.db("eShopBD").collection("products");
+    const pendingOrderCollection = client.db("eShopBD").collection("pendingOrder");
+    const deliverOrder = client.db("eShopBD").collection('deliver');
+    const contactUS = client.db("eShopBD").collection('ContactUS');
+    const solveContactUS = client.db("eShopBD").collection('solveContact');
+
+    //GET PRODUCTS
+    app.post('/products', async (req, res) => {
+      const newProduct = req.body
+      const result = await productsCollection.insertOne(newProduct);
+      res.send({ result, message: "User Added In Database" });
+    })
+
+    app.get('/products', async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    })
+
+    //PLACE ORDER
+    app.post('/pendingOrderData', async (req, res) => {
+      const pendingOrderDataPost = req.body;
+      trackOrder = trackOrder + 1;
+      const orderData = await pendingOrderCollection.insertOne({ ...pendingOrderDataPost, trackOrder });
+      res.send({ orderData, orderID: `${trackOrder}` });
+    });
+    // GET ALL PENDING ORDERS
+    app.get('/pendingOrderData', async (req, res) => {
+      const data = await pendingOrderCollection.find().toArray();
+      res.send(data);
+    });
 
 
-
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
+
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
@@ -50,4 +90,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`e-shop-bd Server is running on port : ${port}`)
-})
+});
