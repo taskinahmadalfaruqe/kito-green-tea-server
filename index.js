fd6c7ec4ed5ba0cbae2a2a4b784d67e28c28620c
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-05-14 16:46:36 +0600

    [fully ready]: now server is ready with all action. ready to deploy

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ const corsConfig = {
 app.use(cors())
 app.use(express.json());
 
-// const uri = "mongodb+srv://taskinahmadalfaruqe:SbqShhMkstQwducO@cluster0.5tob3mx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://taskinahmadalfaruqe:SbqShhMkstQwducO@cluster0.5tob3mx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const uri = "mongodb+srv://e-shop-bd:UBShkNROi2oq4o2p@cluster0.1gxcng8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const uri = "mongodb+srv://e-shop-bd:UBShkNROi2oq4o2p@cluster0.1gxcng8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -42,7 +42,8 @@ const client = new MongoClient(uri, {
 });
 
 let CustomOrder = 100;
-const GetTime = new Date().getSeconds();
+const GetSeconds = new Date().getSeconds();
+const GetMinutes = new Date().getMinutes();
 const GetMonth = new Date().getMonth();
 const GetDate = new Date().getDate();
 const GetYear = new Date().getFullYear();
@@ -51,22 +52,32 @@ const GetYear = new Date().getFullYear();
 async function run() {
   try {
     // await client.connect();
+    // ALL COLLECTION FOR OUR DATA SERVER
     const productsCollection = client.db("eShopBD").collection("products");
     const pendingOrderCollection = client.db("eShopBD").collection("pendingOrder");
     const adminCollection = client.db("eShopBD").collection('adminCollection');
     const pendingContactData = client.db("eShopBD").collection('pendingContact');
 
-    //GET PRODUCTS
+
+
+
+    // =============================================
+    //                PRODUCT ACTION
+    // =============================================
+
+
+    //POST A PRODUCT DATA 
     app.post('/products', async (req, res) => {
       const newProduct = req.body
       const result = await productsCollection.insertOne(newProduct);
       res.send({ result, message: "User Added In Database" });
     })
-
+    // GET ALL PRODUCTS AS A ARRAY
     app.get('/products', async (req, res) => {
       const result = await productsCollection.find().toArray();
       res.send(result);
     })
+    // GET A SINGLE PRODUCT BY ID
     app.get('/products/:id', async (req, res) => {
       const id = req.params.id;
       const result = await productsCollection.findOne({ _id: new ObjectId(id) });
@@ -92,15 +103,22 @@ async function run() {
       res.send(result)
     })
 
-    //PLACE ORDER
+
+
+    // =============================================
+    //                ORDER ACTION
+    // =============================================
+
+
+    //PLACE A ORDER
     app.post('/pendingOrderData', async (req, res) => {
       const pendingOrderDataPost = req.body;
       CustomOrder = CustomOrder + 1;
-      const trackOrder = `${GetYear}${GetMonth + 1}${GetDate}${GetTime}${CustomOrder}`
+      const trackOrder = `${GetYear}${GetMonth + 1}${GetDate}${GetMinutes}${GetSeconds}${CustomOrder}`
       const orderData = await pendingOrderCollection.insertOne({ ...pendingOrderDataPost, trackOrder });
       res.send({ orderData, orderID: `${trackOrder}` });
     });
-
+    //GET SINGLE PENDING ORDER BY ID
     app.patch('/pendingOrderData/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
@@ -114,28 +132,37 @@ async function run() {
       const result = await pendingOrderCollection.updateOne(filter, updateDoc, options);
       res.send(result)
     })
-
     // GET ALL PENDING ORDERS
     app.get('/pendingOrderData', async (req, res) => {
       const data = await pendingOrderCollection.find().toArray();
       res.send(data);
     });
 
-    // CONTACT DATA
+
+
+    // =============================================
+    //                CONTACT ACTION
+    // =============================================
+
+
+    // SUBMIT A  CONTACT DATA 
     app.post('/pendingContact', async (req, res) => {
       const data = req.body;
       const result = await pendingContactData.insertOne(data);
       res.send(result);
     })
+    // GET ALL CONTACT DATA 
     app.get('/pendingContact', async (req, res) => {
       const findData = await pendingContactData.find().toArray();
       res.send(findData);
     })
+    //GET A CONTACT DATA BY ID
     app.get('/pendingContact/:id', async (req, res) => {
       const id = req.params.id;
       const findData = await pendingContactData.findOne({ _id: new ObjectId(id) });
       res.send(findData);
     })
+    //UPDATE A CONTACT DATA PENDING TO RESOLVE
     app.patch('/pendingContact/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
@@ -149,14 +176,21 @@ async function run() {
       res.send(result)
     })
 
-    //ADMIN COLLECTION
+
+
+    // =============================================
+    //                ADMIN ACTION
+    // =============================================
+
+
+    // POST A  NEW USER DATA IN ADMIN COLLECTION
     app.post('/adminCollection', async (req, res) => {
       const adminEmail = req.body;
       console.log(adminEmail);
       const result = await adminCollection.insertOne(adminEmail);
       res.send(result)
     })
-    //
+    // VERIFY IS HE OR SHE ADMIN
     app.get('/adminCollection/:email', async (req, res) => {
       const email = req.params.email;
       const query = { email: email }
@@ -167,7 +201,7 @@ async function run() {
       }
       res.send(admin)
     })
-
+    //GET ALL USER DATA
     app.get('/adminCollection', async (req, res) => {
       const findAdmin = await adminCollection.find().toArray();
       res.send(findAdmin)
